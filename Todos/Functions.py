1dff1e421966a1d60585c83b0e880f988b936691
saumya <saumyaagarwal65@gmail.com>
2023-10-07 17:12:34 +0530

    Add if name==main condition

--- Todos/Functions.py
+++ Todos/Functions.py
@@ -1,4 +1,6 @@
-def open_read(filepath="todos_run.txt"):
+FILEPATH="todos_run.txt"
+
+def open_read(filepath=FILEPATH):
     """
     open todos text file and read its content
     """
@@ -6,7 +8,12 @@ def open_read(filepath="todos_run.txt"):
         todos_local = file.readlines()
         return (todos_local)
 
-def open_write(todos_arg, filepath="todos_run.txt"):
+def open_write(todos_arg, filepath=FILEPATH):
     """ open todos text file and change its content"""
     with open(filepath, 'w') as file:
         file.writelines(todos_arg)
+
+if __name__=="__main__":
+    print("This is functions file which you are running directly without main file.")
+    print("If you used main file to call this file, its name would be Functions but if "
+          "you directly call it, its name is main with __...__")
