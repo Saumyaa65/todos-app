187dcf2014a0e4c0b049730954472f02c4d97592
saumya <saumyaagarwal65@gmail.com>
2023-10-28 19:03:25 +0530

    Implement Add and Edit buttons in ToDo GUI

--- Todos/gui.py
+++ Todos/gui.py
@@ -2,9 +2,40 @@ import Functions
 import PySimpleGUI as gui
 
 label=gui.Text("Type a todo")
-box=gui.InputText(tooltip="Enter todo")
-add=gui.Button("ADD")
+box=gui.InputText(tooltip="Enter todo", key="todo")
+add=gui.Button("Add")
+edit=gui.Button("Edit")
+list_box=gui.Listbox(values=Functions.open_read(), key="todos",
+                     enable_events=True, size=[45,10])
 
-window=gui.Window("To-Dos", layout=[[label],[box],[add]])
-window.read()
+window=gui.Window("To-Dos",
+                  layout=[[label],[box],[add],
+                          [list_box,edit]],
+                  font=("Arial",15))
+
+while True:
+    event, values=window.read()
+    print(event)
+    print(values)
+    match event:
+        case "Add":
+            todos= Functions.open_read()
+            new_todo=values["todo"]+"\n"
+            todos.append(new_todo)
+            Functions.open_write(todos)
+            window["todos"].update(values=todos)
+        case gui.WIN_CLOSED:
+            break
+        case "Edit":
+            todo_edit=values["todos"][0]
+            new_todo=values["todo"]+"\n"
+            todos=Functions.open_read()
+            index=todos.index(todo_edit)
+            todos[index]=new_todo
+            Functions.open_write(todos)
+            window["todos"].update(values=todos)
+        case "todos":
+            todo=values["todos"][0]
+            window["todo"].update(value=todo)
+
 window.close()
