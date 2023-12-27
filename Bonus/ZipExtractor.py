603b6bc2b81d051437f82a5b596b8a39c58a93e2
Saumyaa65 <148757540+Saumyaa65@users.noreply.github.com>
2023-12-27 21:15:18 +0530

    Delete Bonus/ZipExtractor.py

--- Bonus/ZipExtractor.py
+++
@@ -1,8 +0,0 @@
-import zipfile
-def extract_archive(path, des):
-    with zipfile.ZipFile(path, 'r') as file:
-        file.extractall(des)
-
-if __name__=="__main__":
-    extract_archive("D:\Python\Bonus\compressed.zip",
-                    "E:\Saumya")
